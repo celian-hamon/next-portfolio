1f10965a7d2fed0ee981d6f2649b71bb32bfacab
celian-hamon <skelletondude@gmail.com>
2022-07-01 21:40:23 +0200

    🤟 Link optimisation

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -3,7 +3,11 @@ import styles from '../styles/Home.module.scss'
 
 export default function ProjectCard({ project }) {
     return (
-        <Link href={`/${project.id}`}>
+        <Link href={{
+            pathname: '/projects/[id]',
+            query: { id: project.id },
+        }} 
+        replace>
             <div className={`${styles.project}`}>
                 <h2>Name : {project.name}</h2>
                 <h3>Role : {project.role.join(', ')}</h3>
