b496c1d66dcd03bc9686765964fb9e6be9ae1954
celian-hamon <skelletondude@gmail.com>
2022-07-01 21:21:02 +0200

    🪛 added title and increased font weigth on project hover

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.scss'
+import Head from 'next/head'
 
 import TransitionLayout from "../components/TransitionLayout";
 function MyApp({ Component, pageProps }) {
   return (
     <TransitionLayout>
+      <Head>
+        <title>Célian HAMON | Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </TransitionLayout>
   );
